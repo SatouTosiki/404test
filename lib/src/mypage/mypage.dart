b8b2e5d84f346314deaec10539bc9906df9fd6f8
tosiki <tosiki5880@gamil.com>
2023-07-12 15:12:39 +0900

    userpage変更

--- lib/src/mypage/mypage.dart
+++ lib/src/mypage/mypage.dart
@@ -13,47 +13,56 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final Random random = Random(); // 追加
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(
-      //     '${user?.displayName ?? ''}のpage',
-      //     style: GoogleFonts.happyMonkey(
-      //       textStyle: const TextStyle(fontSize: 30),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${user?.displayName ?? ''} is room',
+              style: GoogleFonts.happyMonkey(
+                textStyle: const TextStyle(fontSize: 25, color: Colors.black),
+              ),
+            ),
+            IconButton(
+              icon: Icon(LineIcons.android),
+              color: Colors.black,
+              onPressed: () {
+                // ログアウト処理
+                FirebaseAuth.instance.signOut();
+                Navigator.pop(context); // マイページ画面を閉じてログイン画面に戻る
+              },
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+      ),
+      // body: SingleChildScrollView(
+      //   child: Padding(
+      //     padding: const EdgeInsets.all(10),
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceBetween, // 左右に要素を配置
+      //       children: [
+      //         Text(
+      //           '${user?.displayName ?? ''} is room',
+      //           style: GoogleFonts.happyMonkey(
+      //               textStyle: const TextStyle(fontSize: 25)),
+      //         ),
+      //         IconButton(
+      //           icon: Icon(LineIcons.android),
+      //           onPressed: () {
+      //             // ログアウト処理
+      //             FirebaseAuth.instance.signOut();
+      //             Navigator.pop(context); // マイページ画面を閉じてログイン画面に戻る
+      //           },
+      //           // child: const Text(
+      //           //   'ログアウト',
+      //           //   style: TextStyle(fontSize: 10),
+      //           // ),
+      //         ),
+      //       ],
       //     ),
       //   ),
-      //   backgroundColor: Color.fromARGB(
-      //     255,
-      //     random.nextInt(256),
-      //     random.nextInt(256),
-      //     random.nextInt(256),
-      //   ), // ランダムな色を設定
       // ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween, // 左右に要素を配置
-            children: [
-              Text(
-                '${user?.displayName ?? ''} is room',
-                style: GoogleFonts.happyMonkey(
-                    textStyle: const TextStyle(fontSize: 25)),
-              ),
-              IconButton(
-                icon: Icon(LineIcons.android),
-                onPressed: () {
-                  // ログアウト処理
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pop(context); // マイページ画面を閉じてログイン画面に戻る
-                },
-                // child: const Text(
-                //   'ログアウト',
-                //   style: TextStyle(fontSize: 10),
-                // ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
