ee9a1ae0339b132871b4500a0e40a3badbc49235
tosiki <tosiki5880@gamil.com>
2023-08-09 14:18:58 +0900

    投稿ページ変更

--- lib/src/push/oush.dart
+++ lib/src/push/oush.dart
@@ -26,15 +26,15 @@ class _NotificationScreen extends State<NotificationScreen> {
   List<File> images = []; // 選択された複数の画像を格納するリスト
   final picker = ImagePicker();
 
-  // カメラで撮影した画像を取得する命令
-  // Future getImageFromCamera() async {
-  //   final pickedFile = await picker.pickImage(source: ImageSource.camera);
-  //   setState(() {
-  //     if (pickedFile != null) {
-  //       images.add(File(pickedFile.path)); // images リストに画像を追加
-  //     }
-  //   });
-  // }
+  //カメラで撮影した画像を取得する命令
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        images.add(File(pickedFile.path)); // images リストに画像を追加
+      }
+    });
+  }
 
   // 端末のアルバムに保存されている画像を取得する命令
   Future getImageFromGallery() async {
@@ -60,24 +60,55 @@ class _NotificationScreen extends State<NotificationScreen> {
         child: Center(
           child: Column(
             children: [
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
+              images.isNotEmpty
+                  ? const Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        "画像が選択されています",
+                        style: TextStyle(
+                            //fontStyle: FontStyle.italic,
+                            fontSize: 20),
+                      ),
+                    )
+                  : const Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        "画像は5枚まで^_^",
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic, fontSize: 20),
+                      ),
+                    ),
+              FilledButton.tonal(
+                child: Text(
+                  '写真を撮る',
+                  style: TextStyle(fontSize: 20),
+                ),
                 onPressed: () {
-                  // getImageFromCamera();
+                  getImageFromCamera();
                 },
-                child: const Text('写真を撮る'),
+                style: FilledButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
               ),
-              const SizedBox(height: 5),
-              ElevatedButton(
+
+              const SizedBox(height: 25),
+
+              FilledButton.tonal(
+                child: const Text(
+                  'ライブラリから選ぶ',
+                  style: TextStyle(fontSize: 20),
+                ),
                 onPressed: () {
                   getImageFromGallery();
                 },
-                child: const Text('アルバムから選ぶ'),
+                style: FilledButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
               ),
+
               const SizedBox(height: 20),
               // 選択された画像を表示
+
               Column(
                 children: images.map((image) {
                   return Padding(
@@ -90,6 +121,27 @@ class _NotificationScreen extends State<NotificationScreen> {
                   );
                 }).toList(),
               ),
+              const SizedBox(height: 40),
+
+              if (images.isNotEmpty) // 画像が選択されている場合にのみ表示
+
+                OutlinedButton(
+                  child: Text(
+                    '次へ',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    fixedSize: Size(200, 50), // 幅200、高さ50の大きさに設定
+
+                    primary: Colors.black,
+                    shape: const StadiumBorder(),
+                    side: const BorderSide(color: Colors.orange),
+                  ),
+                  onPressed: () {},
+                ),
+              const SizedBox(
+                height: 50,
+              )
             ],
           ),
         ),
