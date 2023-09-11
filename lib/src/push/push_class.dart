f536e5d7cd54c32301222bdc9e5e2e9e2d77762f
tosiki <tosiki5880@gamil.com>
2023-09-11 14:55:40 +0900

    a

--- lib/src/push/push_class.dart
+++ lib/src/push/push_class.dart
@@ -1,34 +1,49 @@
 import 'push.dart';
+
 import 'package:flutter/material.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:firebase_storage/firebase_storage.dart';
+
 import 'package:flutter/material.dart';
+
 import 'package:image_picker/image_picker.dart';
 
 // ユーザーがログインしていることを確認する関数
+
 Future<User?> getCurrentUser() async {
   return FirebaseAuth.instance.currentUser;
 }
 
 class AddBookModel extends ChangeNotifier {
   String? title;
+
   String? author;
+
   List<File> imageFiles = []; // 複数の画像ファイルのパスを格納するリスト
+
   String? timestamp;
+
   bool isLoading = false;
 
   final picker = ImagePicker();
 
   void startLoading() {
     isLoading = true;
+
     notifyListeners();
   }
 
   void endLoading() {
     isLoading = false;
+
     notifyListeners();
   }
 
@@ -45,14 +60,18 @@ class AddBookModel extends ChangeNotifier {
 
     for (var imageFile in imageFiles) {
       // storageにアップロード
+
       final task = await FirebaseStorage.instance
           .ref('user_post/${doc.id}/${imageFile.path}')
           .putFile(imageFile);
+
       final imgURL = await task.ref.getDownloadURL();
+
       imgURLs.add(imgURL);
     }
 
     // firestoreに追加
+
     await doc.set({
       'title': title,
       'author': author,
@@ -65,11 +84,14 @@ class AddBookModel extends ChangeNotifier {
 
   Future pickImage() async {
     final List<XFile>? pickedFiles = await picker.pickMultiImage();
+
     if (pickedFiles != null) {
       for (var pickedFile in pickedFiles) {
         if (imageFiles.length < 5) {
           String imagePath = pickedFile.path;
+
           imageFiles.add(File(imagePath)); // 画像ファイルのパスをリストに追加」
+
           int length = imageFiles.length;
         } else {
           print("画像が多い");
@@ -86,82 +108,157 @@ class AddBookModel extends ChangeNotifier {
 class HexColor extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll('#', '');
+
     if (hexColor.length == 6) {
       hexColor = 'FF' + hexColor;
     }
+
     return int.parse(hexColor, radix: 16);
   }
 
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
 
+ 
+
 //colorクラス
 
+ 
+
 // class RecipeForm extends StatelessWidget {
+
 //   @override
+
 //   Widget build(BuildContext context) {
+
 //     return Column(
+
 //       children: const [
+
 //         //Text("レシピを追加"),
+
 //         TextField(
+
 //           decoration: InputDecoration(hintText: "タイトル"),
+
 //         ),
+
 //         TextField(
+
 //           decoration: InputDecoration(hintText: "説明"),
+
 //         ),
+
 //       ],
+
 //     );
+
 //   }
+
 // }
+
+ 
 
 // class AddDocumentScreen extends StatelessWidget {
+
 //   final TextEditingController titleController = TextEditingController();
+
 //   final TextEditingController contentController = TextEditingController();
 
+ 
+
 //   void addDocument() {
+
 //     String title = titleController.text;
+
 //     String content = contentController.text;
 
+ 
+
 //     CollectionReference documents =
+
 //         FirebaseFirestore.instance.collection('user_post');
 
+ 
+
 //     documents.add({
+
 //       'title': title,
+
 //       'content': content,
+
 //     }).then((docRef) {
+
 //       print('Document added with ID: ${docRef.id}');
+
 //     }).catchError((error) {
+
 //       print('Error adding document: $error');
+
 //     });
+
 //   }
 
+ 
+
 //   @override
+
 //   Widget build(BuildContext context) {
+
 //     return Scaffold(
+
 //       appBar: AppBar(
+
 //         title: Text('Add Document'),
+
 //       ),
+
 //       body: Padding(
+
 //         padding: EdgeInsets.all(16.0),
+
 //         child: Column(
+
 //           crossAxisAlignment: CrossAxisAlignment.start,
+
 //           children: [
+
 //             TextField(
+
 //               controller: titleController,
+
 //               decoration: InputDecoration(labelText: 'Title'),
+
 //             ),
+
 //             TextField(
+
 //               controller: contentController,
+
 //               decoration: InputDecoration(labelText: 'Content'),
+
 //             ),
+
 //             SizedBox(height: 16.0),
+
 //             ElevatedButton(
+
 //               onPressed: addDocument,
+
 //               child: Text('Add Document'),
+
 //             ),
+
 //           ],
+
 //         ),
+
 //       ),
+
 //     );
+
 //   }
+
 // }
+
+ 
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -15,8 +16,7 @@ class _YourScreenState extends State<YourScreen> {
   @override
   void initState() {
     super.initState();
-    fetchDocumentData(); // 初期データの取得
-    subscribeToUpdates(); // リアルタイム更新の購読
+    //fetchDocumentData(); // 初期データの取得
   }
 
   Future<void> fetchDocumentData() async {
@@ -27,9 +27,11 @@ class _YourScreenState extends State<YourScreen> {
           .get();
 
       List<Map<String, dynamic>> dataList = [];
+
       querySnapshot.docs.forEach((doc) {
         if (doc.exists) {
           Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
           dataList.add(data);
         }
       });
@@ -42,116 +44,119 @@ class _YourScreenState extends State<YourScreen> {
     }
   }
 
-  void subscribeToUpdates() {
-    firestore.collection('user_post').snapshots().listen((event) {
-      fetchDocumentData(); // データが変更されたときにデータを更新
-    });
+  Future<void> _refreshData() async {
+    await fetchDocumentData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
+      body: RefreshIndicator(
+        onRefresh: _refreshData,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
 
-            // ドキュメントのリストを表示
-            Column(
-              children: documentList.map<Widget>((documentData) {
-                return Container(
-                  // 各投稿を囲むContainerを追加
-                  margin: EdgeInsets.all(10), // 枠の余白を追加
-                  padding: EdgeInsets.all(10), // 内容の余白を追加
-                  decoration: BoxDecoration(
-                    // 枠のスタイルを設定
-                    border: Border.all(
-                      // 黒い枠線を追加
-                      color: Colors.blue,
-                      width: 2.0,
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      // 画像URLリストを表示
-                      if (documentData['imgURL'] is List)
-                        Column(
-                          children:
-                              documentData['imgURL'].map<Widget>((imageUrl) {
-                            return Image.network(
-                              imageUrl,
-                            );
-                          }).toList(),
-                        )
-                      else if (documentData['imgURL'] is String)
-                        Image.network(
-                          documentData['imgURL'],
-                        ), // 単一のURLの場合
-                      RichText(
-                        textAlign: TextAlign.center, // テキストをセンターに配置
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'title\n',
-                              style: GoogleFonts.happyMonkey(
-                                textStyle: const TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            TextSpan(
-                              text: ' ${documentData['title']}',
-                              style: TextStyle(
-                                fontSize: 20, // 異なるスタイルを設定
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-
-                      const Divider(
-                        height: 10,
-                        thickness: 2,
-                        indent: 10,
-                        endIndent: 0,
+              // ドキュメントのリストを表示
+              Column(
+                children: documentList.map<Widget>((documentData) {
+                  return Container(
+                    margin: EdgeInsets.all(5), // 枠の余白を追加
+                    padding: EdgeInsets.all(5), // 内容の余白を追加
+                    decoration: BoxDecoration(
+                      // 枠のスタイルを設定
+                      border: Border.all(
+                        // 黒い枠線を追加
                         color: Colors.blue,
+                        width: 2.0,
                       ),
-                      RichText(
-                        textAlign: TextAlign.center, // テキストをセンターに配置
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'comment\n',
-                              style: GoogleFonts.happyMonkey(
-                                textStyle: const TextStyle(
-                                  fontSize: 30,
+                      borderRadius: BorderRadius.all(
+                        // 角を丸くする
+                        Radius.circular(20),
+                      ),
+                    ),
+
+                    child: Column(
+                      children: [
+                        if (documentData['imgURL'] is List)
+                          Column(
+                            children:
+                                documentData['imgURL'].map<Widget>((imageUrl) {
+                              return Image.network(
+                                imageUrl,
+                              );
+                            }).toList(),
+                          )
+                        else if (documentData['imgURL'] is String)
+                          Image.network(
+                            documentData['imgURL'],
+                          ),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'title\n',
+                                style: GoogleFonts.happyMonkey(
+                                  textStyle: TextStyle(
+                                    fontSize: 30,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' ${documentData['title']}',
+                                style: TextStyle(
+                                  fontSize: 20,
                                   color: Colors.black,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),
-                            TextSpan(
-                              text: ' ${documentData['author']}',
-                              style: TextStyle(
-                                fontSize: 20, // 異なるスタイルを設定
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
-          ],
+                        const Divider(
+                          height: 10,
+                          thickness: 2,
+                          indent: 10,
+                          endIndent: 0,
+                          color: Colors.blue,
+                        ),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'comment\n',
+                                style: GoogleFonts.happyMonkey(
+                                  textStyle: TextStyle(
+                                    fontSize: 30,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' ${documentData['author']}',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
